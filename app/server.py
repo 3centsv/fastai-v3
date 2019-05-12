0b90c38b0c80c543267febd062254d5d30f13197
3centsv <50065526+3centsv@users.noreply.github.com>
2019-05-12 10:53:22 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/file/d/1OgOZAUrDDj0Hf0GHeih5jIguv24lfeX-/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=14G2vh0DQudiDM_r3czq6uoQ5G4N6rB5g'
 export_file_name = 'export.pkl'
 
-classes = ['Assam_Silk', 'Banarasi_Silk', 'Bandhani']
+classes = ['Banarasi_Silk','Bandhani','Bhagalpuri_Silk','Bomkai']
 path = Path(__file__).parent
 
 app = Starlette()
